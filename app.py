1a75f554055c32fcf32fc53a8bf3ec0c9dd3b18d
Hesham Eldesoky <hishameldesoky111@gmail.com>
2025-05-12 18:57:09 +0300

    the model is added

---
+++ app.py
@@ -0,0 +1,7 @@
+from flask import Flask, render_template, request , jsonify
+from pickle 
+import numpy as np 
+import pandas as pd 
+from sklearn.preprocessing import StandardScaler
+from sklearn.impute import SimpleImputer
+
